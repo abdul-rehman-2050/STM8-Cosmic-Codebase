c4ba294e6e6b1e3dc600265ca1b2f1b525ec8465
Abdul Rehman <abdul.rehman.vud@gmail.com>
2019-03-19 15:48:14 +0500

    code indented

--- blinky.c
+++ blinky.c
@@ -1,14 +1,14 @@
-#include <iostm8s103.h>
+	#include <iostm8s103.h>
 	
 main()
 {
 	unsigned int d;
-	 PB_DDR = (1<<5);
-	 PB_CR1 = (1<<5);
+	PB_DDR = (1<<5);
+	PB_CR1 = (1<<5);
 	while (1){
-		for (d = 0; d < 40000; d++); //Delay ~ 1 second
-			PB_ODR |= (1<<5);
-		for (d = 0; d < 40000; d++); //Delay ~ 1 second
-      PB_ODR &= ~(1<<5);
+	   for (d = 0; d < 40000; d++); //Delay ~ 1 second
+	   PB_ODR |= (1<<5);
+	   for (d = 0; d < 40000; d++); //Delay ~ 1 second
+      	   PB_ODR &= ~(1<<5);
 	}
 }
